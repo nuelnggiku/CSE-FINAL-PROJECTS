7c7ed18b38162c7f0fb3f7fbbb5289b934c0a5d3
nuelnggiku <ngg22001@byui.edu>
2022-10-19 18:33:46 +0700

    Game Baru menarik

--- finalproject.py
+++ finalproject.py
@@ -1,43 +1,7 @@
-def letters(alphabet):
-    """ 
- Check the provided text to see whether any letters are repeated.
-A set should be used in this solution, and the performance should be O(n).
-    """
-    found = set()
-    for letter in alphabet:
-        # Check the set to see if the letter has already been seen.
-        if letter in found:
-            return False
-        # If not, we shall include it in the set and continue.
-        found.add(letter)
-    # All of the letters were unique if we were able to get this far.
-    return True
+import random
 
-# Alternative course of action
-def letters2(alphabet):
-    """ 
-    Check the provided alphabet to see whether any letters are repeated.
-A set should be used in this solution, and the performance should be O(n).
-    """
-    unique = set()
-    for letter in alphabet:
-        unique.add(letter)
-#The three lines above can be condensed to only unique = set (alphabet)
-
-# The unique set should be used if the text solely contains unique letters.
-# equal to the length of the alphabet string.
-
-    return len(unique) == len(alphabet)
-
-# This function might be condensed to a single line:
-# if set(alphabet) == len, return len (alphabet)
-
-
-alphabet1 = "akutidaksukakamubangets" # Expect True as each letter is distinct.
-print(letters (alphabet1))
-
-alphabet2 = "akutidaksukakamubangets"  # Because 'a' is repeated, anticipate False.
-print(letters(alphabet2))
-
-alphabet3 = "" 
-print(letters(alphabet3))          # Given that it is an empty string, expect True.
+target_num, guess_num = random.randint(1,20 ), 0
+while target_num != guess_num:
+    guess_num = int(input('Guess a number between 1 and 15 until you get it right : '))
+print('You have got $10.000')
+print("Congratulation")
